5ff698c39fbd472420adbd079a6e4ab7c946f370
Arpit Mishra <arpitmishra4779@gmail.com>
2020-05-08 21:00:48 +0530

    Template for faster I/O

---
+++ CodeChef DSA Learning/cpptemp.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+#define ll long long int
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	ll t;
+	cin >> t;
+	while(t--){
+	    // your code goes here
+	}
+	return 0;
+}
