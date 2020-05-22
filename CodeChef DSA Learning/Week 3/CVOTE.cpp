28301989d1c0ee14141717d1c4aab8a90247352c
Arpit Mishra <arpitmishra4779@gmail.com>
2020-05-23 00:43:43 +0530

    Create CVOTE.cpp

---
+++ CodeChef DSA Learning/Week 3/CVOTE.cpp
@@ -0,0 +1,57 @@
+//https://www.codechef.com/LRNDSA03/problems/CVOTE
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll n,m;
+    cin>>n>>m;
+    unordered_map <string, string> cfct;
+    unordered_map <string, ll> cf, ct;
+    for(ll i=0;i<n;i++)
+    {
+        string name,country;
+        cin>>name>>country;
+        cfct.insert({name,country});
+    }
+    while(m--)
+    {
+        string email;
+        cin>>email;
+        cf[email]++;
+        ct[cfct[email]]++;
+    }
+    pair<string, ll> max={"\0",INT_MIN};
+    for(auto & i : ct)
+    {
+        if(i.second>max.second)
+        {
+            max=make_pair(i.first,i.second);
+        }
+        else if(i.second==max.second)
+        {
+            if(i.first<max.first)
+                max=make_pair(i.first,i.second);
+        }
+    }
+    cout<<max.first<<"\n";
+    max={"\0",INT_MIN};
+    for(auto & i : cf)
+    {
+        if(i.second>max.second)
+        {
+            max=make_pair(i.first,i.second);
+        }
+        else if(i.second==max.second)
+        {
+            if(i.first<max.first)
+            {
+                max=make_pair(i.first,i.second);
+            }
+        }
+    }
+    cout<<max.first;
+}
