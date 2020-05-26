ae151ee6ef595093521c68d27871411acb58b049
Arpit Mishra <arpitmishra4779@gmail.com>
2020-05-27 01:19:00 +0530

    Create C.cpp

---
+++ Codeforces/645/C.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#define ll long long
+#define endl "/n"
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--) {
+        ll a, b, c, d;
+        cin >> a >> b >> c >> d;
+        cout << (c - a)*(d - b)+1 << endl;
+    }
+}
