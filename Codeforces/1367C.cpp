2fe125f881c32fcd340faca03f425034ddf489df
Arpit Mishra <arpitmishra4779@gmail.com>
2020-06-16 22:52:45 +0530

    Create 1367C.cpp

---
+++ Codeforces/1367C.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define ff              first
+#define ss              second
+#define int             long long
+#define pb              push_back
+#define mp              make_pair
+#define pii             pair<int,int>
+#define vi              vector<int>
+#define mii             map<int,int>
+#define pqb             priority_queue<int>
+#define pqs             priority_queue<int,vi,greater<int> >
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+#define mod             1000000007
+#define inf             1e18
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define mk(arr,n,type)  type *arr=new type[n];
+#define w(x)            int x; cin>>x; while(x--)
+mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+
+int32_t main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	int t;
+	cin >> t;
+	while (t--) {
+		int n, k;
+		cin >> n >> k;
+		int m = n / (k + 1);
+		if (n % (k + 1))
+			m += 1;
+		string s;
+		cin >> s;
+		int c = 0;
+		int x = 0;
+		if (s[0] == '0')
+			c++;
+		for (int i = 1; i < n; i++) {
+			if (s[i] == '1')
+			{
+				c -= (i - x <= k);
+				x = i;
+			}
+			else if (i - x > k)
+			{
+				c += (i - x > k);
+				x = i;
+			}
+		}
+		cout << c << '\n';
+	}
+	return 0;
+}
