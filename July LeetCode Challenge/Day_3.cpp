37fe5e20d19b8f4585727826afa87b0c9781a4cc
Arpit Mishra <arpitmishra4779@gmail.com>
2020-07-03 14:25:19 +0530

    Create Day_3.cpp

---
+++ July LeetCode Challenge/Day_3.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+void print_vector(vector<int> v){
+   cout << "[";
+   for(int i = 0; i<v.size(); i++){
+      cout << v[i] << ", ";
+   }
+   cout << "]"<<endl;
+}
+class Solution {
+   public:
+   vector<int> prisonAfterNDays(vector<int>& cells, int N) {
+      map <int, vector <int> > m;
+      if(N == 0) return cells;
+      set <vector <int> > visited;
+      visited.insert(cells);
+      for(int i = 1; i<=14 ; i++ ){
+         vector <int> temp(8);
+         for(int j = 1; j < 7; j++){
+            if(cells[j - 1] ^ cells[j + 1] == 0){
+               temp[j] = 1;
+            }
+         }
+         cells = temp;
+         m[i] = temp;
+         visited.insert(temp);
+      }
+      return m[N % 14 == 0? 14 : N % 14];
+   }
+};
+main(){
+   vector<int> v1 = {0,1,0,1,1,0,0,1};
+   Solution ob;
+   print_vector(ob.prisonAfterNDays(v1, 7));
+}
