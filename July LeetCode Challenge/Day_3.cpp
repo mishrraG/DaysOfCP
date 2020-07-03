0d6fccf16d09ca919e1f3ffae4cb0e61e20d20c0
Arpit Mishra <arpitmishra4779@gmail.com>
2020-07-03 14:29:30 +0530

    Update Day_3.cpp

--- July LeetCode Challenge/Day_3.cpp
+++ July LeetCode Challenge/Day_3.cpp
@@ -9,24 +9,23 @@ void print_vector(vector<int> v){
 }
 class Solution {
    public:
-   vector<int> prisonAfterNDays(vector<int>& cells, int N) {
-      map <int, vector <int> > m;
-      if(N == 0) return cells;
+  vector<int> prisonAfterNDays(vector<int>& cells, int N) {
+        map <int, vector <int> > m;
+      if(!N) 
+          return cells;
       set <vector <int> > visited;
       visited.insert(cells);
       for(int i = 1; i<=14 ; i++ ){
          vector <int> temp(8);
          for(int j = 1; j < 7; j++){
-            if(cells[j - 1] ^ cells[j + 1] == 0){
-               temp[j] = 1;
-            }
+            temp[j]=!(cells[j - 1] ^ cells[j + 1]);
          }
          cells = temp;
          m[i] = temp;
          visited.insert(temp);
       }
-      return m[N % 14 == 0? 14 : N % 14];
-   }
+      return m[N % 14 == 0? 14 : N % 14];        
+    }
 };
 main(){
    vector<int> v1 = {0,1,0,1,1,0,0,1};
