7a1ac8f2bbb317911d414d026c9f62aa1a43d25d
Arpit Mishra <arpitmishra4779@gmail.com>
2020-07-13 20:10:51 +0530

    Create Day_13.cpp

---
+++ July LeetCode Challenge/Day_13.cpp
@@ -0,0 +1,6 @@
+class Solution {
+public:
+	bool isSameTree(TreeNode* p, TreeNode* q) {
+		return !p && !q || p && q && p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+	}
+};
