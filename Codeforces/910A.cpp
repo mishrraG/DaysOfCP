b8ff5f291762069a81df5b2db66ce226006ad750
Arpit Mishra <arpitmishra4779@gmail.com>
2020-08-04 02:49:17 +0530

    Create 910A.cpp

---
+++ Codeforces/910A.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ff              first
+#define ss              second
+#define int             long long
+#define pb              push_back
+#define mp              make_pair
+#define pii             pair<int,int>
+#define vi              vector<int>
+#define mii             map<int,int>
+#define pqb             priority_queue<int>
+#define pqs             priority_queue<int,vi,greater<int> >
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+#define mod             1000000007
+#define inf             1e18
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define mk(arr,n,type)  type *arr=new type[n];
+#define w(x)            int x; cin>>x; while(x--)
+mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+int32_t main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	int n, d;
+	cin >> n >> d;
+	string s;
+	cin >> s;
+	int x = 0, y = 0;
+	while (x < n - 1 && y <= n)
+	{
+		if (s[x] == '1')
+		{
+			x += d;
+			y += 1;
+		}
+		else
+			x -= 1;
+	}
+	if (y < n)
+		cout << y;
+	else
+		cout << "-1";
+
+	return 0;
+}
