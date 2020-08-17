5b4d52e94d0ec18eb0947a03a06b157be7581cdd
Arpit Mishra <arpitmishra4779@gmail.com>
2020-08-17 15:56:39 +0530

    Update LINCHESS.cpp

--- Codechef August Long 2020/LINCHESS.cpp
+++ Codechef August Long 2020/LINCHESS.cpp
@@ -1,1 +1,53 @@
-//Sorry, you've been tricked
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ff              first
+#define ss              second
+#define int             long long
+#define pb              push_back
+#define mp              make_pair
+#define pii             pair<int,int>
+#define vi              vector<int>
+#define mii             map<int,int>
+#define pqb             priority_queue<int>
+#define pqs             priority_queue<int,vi,greater<int> >
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+#define mod             1000000007
+#define inf             1e18
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define mk(arr,n,type)  type *arr=new type[n];
+#define w(x)            int x; cin>>x; while(x--)
+mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+int32_t main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	w(t) {
+		int n, k;
+		cin >> n >> k;
+		vi a(n);
+		int pos = -1;
+		int val = INT_MAX;
+		for (int i = 0; i < n; i++) {
+			cin >> a[i];
+			if (k % a[i] == 0)
+			{
+				int p = k / a[i];
+				if (p < val) {
+					pos = a[i];
+					val = p;
+				}
+			}
+		}
+		cout << pos << '\n';
+
+	}
+	return 0;
+}
