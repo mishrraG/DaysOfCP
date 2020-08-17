7996ad4bfc1d4b3e146ca801e6571ae119130cb7
Arpit Mishra <arpitmishra4779@gmail.com>
2020-08-17 15:55:33 +0530

    Update CRDGAME3.cpp

--- Codechef August Long 2020/CRDGAME3.cpp
+++ Codechef August Long 2020/CRDGAME3.cpp
@@ -1,1 +1,46 @@
-//Sorry, you've been tricked
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ff              first
+#define ss              second
+#define int             long long
+#define pb              push_back
+#define mp              make_pair
+#define pii             pair<int,int>
+#define vi              vector<int>
+#define mii             map<int,int>
+#define pqb             priority_queue<int>
+#define pqs             priority_queue<int,vi,greater<int> >
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+#define mod             1000000007
+#define inf             1e18
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define mk(arr,n,type)  type *arr=new type[n];
+#define w(x)            int x; cin>>x; while(x--)
+mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
+int smallestNumber(int n)
+{
+	return (n % 9 == 0) ? (n / 9) : (n / 9) + 1;
+}
+int32_t main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	w(t) {
+		int c, r;
+		cin >> c >> r;
+		int lc = smallestNumber(c);
+		int lr = smallestNumber(r);
+		if (lc < lr)
+			cout << "0 " << lc << '\n';
+		if (lc >= lr)
+			cout << "1 " << lr << '\n';
+	}
+	return 0;
+}
