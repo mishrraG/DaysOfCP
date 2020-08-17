f2c138c2328ab75a5bc5b8969b6b1f96901ed213
Arpit Mishra <arpitmishra4779@gmail.com>
2020-08-17 16:02:41 +0530

    Update SUBSFREQ.cpp

--- Codechef August Long 2020/SUBSFREQ.cpp
+++ Codechef August Long 2020/SUBSFREQ.cpp
@@ -1,1 +1,62 @@
-//Sorry, you've been tricked
+//10 pts sol :/
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ff              first
+#define ss              second
+#define int             long long
+#define pb              push_back
+#define mp              make_pair
+#define pii             pair<int,int>
+#define vi              vector<int>
+#define mii             map<int,int>
+#define pqb             priority_queue<int>
+#define pqs             priority_queue<int,vi,greater<int> >
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+#define mod             1000000007
+#define inf             1e18
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define mk(arr,n,type)  type *arr=new type[n];
+#define w(x)            int x; cin>>x; while(x--)
+mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+int power(int x, unsigned int y, int p)
+{
+	int res = 1;
+	x = x % p;
+	if (x == 0)
+		return 0;
+	while (y > 0)
+	{
+		if (y & 1)
+			res = (res * x) % p;
+		y = y >> 1;
+		x = (x * x) % p;
+	}
+	return res;
+}
+int32_t main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	w(t) {
+		int n;
+		cin >> n;
+		int a[(int32_t)n];
+		for (int i = 0; i < n; i++)
+		{
+			cin >> a[i];
+		}
+		while (n--) {
+			cout << power(2, n, mod) << ' ';
+		}
+		cout << '\n';
+	}
+	return 0;
+}
