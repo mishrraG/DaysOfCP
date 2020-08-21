9c539ceea01ed0a857afaf7a33214e570ad1024d
Arpit Mishra <arpitmishra4779@gmail.com>
2020-08-21 13:17:21 +0530

    Create Day_20.cpp

---
+++ August LeetCode Challenge/Day_20.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+	vector<int> sortArrayByParity(vector<int>& A) {
+		int l = 0, r = A.size() - 1;
+		while (l < r) {
+			if (A[l] % 2 != 0) {
+				swap(A[l], A[r]);
+				--r;
+			} else {
+				++l;
+			}
+		}
+		return A;
+	}
+};
