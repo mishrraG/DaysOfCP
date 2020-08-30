aa89eec418710fb409f1391935af89080e177b86
Arpit Mishra <arpitmishra4779@gmail.com>
2020-08-30 23:48:36 +0530

    Create 1397A.cpp

---
+++ Codeforces/1397A.cpp
@@ -0,0 +1,76 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ff              first
+#define ss              second
+#define int             long long
+#define pb              push_back
+#define mp              make_pair
+#define pii             pair<int,int>
+#define vi              vector<int>
+#define mii             map<int,int>
+#define umii            unordered_map<int,int>
+#define all(v)          v.begin(),v.end()
+#define pqb             priority_queue<int>
+#define pqs             priority_queue<int,vi,greater<int>>
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+#define mod             1000000007
+#define inf             1e18
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define mk(arr,n,type)  type *arr=new type[n];
+#define w(x)            int x; cin>>x; while(x--)
+#define sortall(x)      sort(all(x))
+#define deb(x)          cout << #x << "="<< x << '\n'
+mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+int power(int , unsigned int);
+int32_t main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    w(t) {
+        unordered_map<char, int> freq;
+        int n;
+        cin >> n;
+        for (int i = 0; i < n; i++)
+        {
+            string s;
+            cin >> s;
+            for (int j = 0; j < s.length(); j++)
+            {
+                freq[s[j]]++;
+            }
+        }
+        bool chk = true;
+        for (auto p : freq)
+        {
+            if (p.ss % n)
+            {
+                chk = false;
+                break;
+            }
+        }
+        cout << (chk ? "YES\n" : "NO\n");
+    }
+    return 0;
+}
+int power(int x, unsigned int y)
+{
+    int res = 1;
+    x = x % mod;
+    if (x == 0) return 0;
+    while (y > 0)
+    {
+        if (y & 1)
+            res = (res * x) % mod;
+        y = y >> 1;
+        x = (x * x) % mod;
+    }
+    return res;
+}
