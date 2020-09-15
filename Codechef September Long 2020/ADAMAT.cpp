7fbadae39d22e6aa4a84364e7bc14b41eadefa0d
Arpit Mishra <arpitmishra4779@gmail.com>
2020-09-15 11:11:12 +0530

    Update ADAMAT.cpp

--- Codechef September Long 2020/ADAMAT.cpp
+++ Codechef September Long 2020/ADAMAT.cpp
@@ -1,1 +1,107 @@
-You could not live with your own failure. And where did that bring you ? Back to me.
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ff              first
+#define ss              second
+#define int             long long
+#define pb              push_back
+#define mp              make_pair
+#define pii             pair<int,int>
+#define vi              vector<int>
+#define mii             map<int,int>
+#define umii			unordered_map<int,int>
+#define all(v)			v.begin(),v.end()
+#define pqb             priority_queue<int>
+#define pqs             priority_queue<int,vi,greater<int>>
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+#define mod             1000000007
+#define inf             1e18
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define mk(arr,n,type)  type *arr=new type[n];
+#define w(x)            int x; cin>>x; while(x--)
+#define sortall(x)		sort(all(x))
+#define deb(x)			cout << #x << "="<< x << '\n'
+#define MAX             65
+mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+int power(int , unsigned int);
+int a[MAX][MAX];;
+
+void transpose(int k)
+{
+	for (int i = 1; i <= k; i++)
+	{
+		for (int j = 1; j <= k; j++)
+		{
+			if (j > i)
+			{
+				int t = a[i][j];
+				a[i][j] = a[j][i];
+				a[j][i] = t;
+			}
+		}
+	}
+}
+
+int32_t main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	w(t) {
+		int n;
+		cin >> n;
+		int aa[MAX][MAX];
+		int val = 1;
+		for (int i = 1; i <= n; i++)
+		{
+			for (int j = 1; j <= n; j++)
+			{
+				aa[i][j] = ((i - 1) * n) + j;
+			}
+		}
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++)
+			{
+				cin >> a[i][j];
+			}
+		}
+		int c = 0;
+		while (true) {
+			bool flag = true;
+			int i = n;
+			while (i >= 1) {
+				if ( a[1][i] != aa[1][i] ) {
+					flag = false;
+					transpose(i);
+					c++;
+					break;
+				}
+				i--;
+			}
+			if (flag)
+				break;
+		}
+		cout << c << '\n';
+	}
+	return 0;
+}
+int power(int x, unsigned int y)
+{
+	int res = 1;
+	x = x % mod;
+	if (x == 0) return 0;
+	while (y > 0)
+	{
+		if (y & 1)
+			res = (res * x) % mod;
+		y = y >> 1;
+		x = (x * x) % mod;
+	}
+	return res;
+}
