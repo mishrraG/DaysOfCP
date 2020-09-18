a3de7f0198196504f90359ce3dd257015cd5249d
Arpit Mishra <arpitmishra4779@gmail.com>
2020-09-18 17:45:54 +0530

    Create SSEC0004.cpp

---
+++ SSCC2020/SSEC0004.cpp
@@ -0,0 +1,91 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ff              first
+#define ss              second
+#define int             long long
+#define pb              push_back
+#define mp              make_pair
+#define pii             pair<int,int>
+#define vi              vector<int>
+#define mii             map<int,int>
+#define umii			unordered_map<int,int>
+#define all(v)			v.begin(),v.end()
+#define pqb             priority_queue<int>
+#define pqs             priority_queue<int,vi,greater<int>>
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+#define mod             1000000007
+#define inf             1e18
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define mk(arr,n,type)  type *arr=new type[n];
+#define w(x)            int x; cin>>x; while(x--)
+#define sortall(x)		sort(all(x))
+#define deb(x)			cout << #x << "="<< x << '\n'
+mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+struct Date
+{
+	int day, month, year;
+};
+
+bool compare(const Date &d1, const Date &d2)
+{
+	// All cases when true should be returned
+	if (d1.year < d2.year)
+		return true;
+	if (d1.year == d2.year && d1.month < d2.month)
+		return true;
+	if (d1.year == d2.year && d1.month == d2.month &&
+		d1.day < d2.day)
+		return true;
+
+	// If none of the above cases satisfy, return false
+	return false;
+}
+void sortDates(Date arr[], int n)
+{
+	sort(arr, arr + n, compare);
+}
+int power(int , unsigned int);
+int32_t main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	int n;
+	cin >> n;
+	Date arr[n];
+	for(int i=0;i<n;i++)
+	{
+		std::cin >> arr[i].day;
+		std::cin.get();
+		std::cin >> arr[i].month;
+		std::cin.get();
+		std::cin >> arr[i].year;
+	}
+	sortDates(arr,n);
+	for(int i=0;i<n;i++)
+	{
+		cout << arr[i].day << "/" <<arr[i].month << "/" <<arr[i].year << "\n";
+	}
+	return 0;
+}
+int power(int x, unsigned int y)
+{
+	int res = 1;
+	x = x % mod;
+	if (x == 0) return 0;
+	while (y > 0)
+	{
+		if (y & 1)
+			res = (res * x) % mod;
+		y = y >> 1;
+		x = (x * x) % mod;
+	}
+	return res;
+}
