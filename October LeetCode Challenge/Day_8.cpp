86e073fcf80da969c30c2770174a5d4d71fd91e6
Arpit Mishra <arpitmishra4779@gmail.com>
2020-10-09 03:26:00 +0530

    Create Day_8.cpp

---
+++ October LeetCode Challenge/Day_8.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+	int search(vector<int>& nums, int target) {
+		int s = 0;
+
+		int e = nums.size() - 1;
+
+		while (s <= e)
+		{
+			int mid = s + (e - s) / 2;
+
+			if (nums[mid] == target) return mid;
+
+			if (target < nums[mid]) e = mid - 1;
+
+			else s = mid + 1;
+		}
+
+		return -1;
+	}
+};
