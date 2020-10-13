4229144a73323cccc4243b9b6716b18bbbd093da
Arpit Mishra <arpitmishra4779@gmail.com>
2020-10-13 17:59:02 +0530

    Update ADDSQUARE.cpp

--- Codechef October Long 2020/ADDSQUARE.cpp
+++ Codechef October Long 2020/ADDSQUARE.cpp
@@ -1,1 +1,111 @@
-Goli beta masti nahi
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ff first
+#define ss second
+#define int long long
+#define pb push_back
+#define mp make_pair
+#define pii pair<int, int>
+#define vi vector<int>
+#define mii map<int, int>
+#define umii unordered_map<int, int>
+#define all(v, x) v, v + x
+#define pqb priority_queue<int>
+#define pqs priority_queue<int, vi, greater<int>>
+#define setbits(x) __builtin_popcountll(x)
+#define zrobits(x) __builtin_ctzll(x)
+#define mod 1000000007
+#define inf 1e18
+#define ps(x, y) fixed << setprecision(y) << x
+#define mk(arr, n, type) type *arr = new type[n];
+#define w(x)  \
+	int x;    \
+	cin >> x; \
+	while (x--)
+#define sortall(x) sort(all(x))
+#define deb(x) cout << #x << "=" << x << '\n'
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+int power(int, unsigned int);
+
+int numberOfSquares(int X[], int Y[], int N, int M)
+{
+	umii m1;
+	int ans = 0;
+	for (int i = 0; i < N; i++)
+		for (int j = i + 1; j < N; j++)
+			m1[abs(X[i] - X[j])]++;
+	umii m2;
+	for (int i = 0; i < M; i++)
+		for (int j = i + 1; j < M; j++)
+			m2[abs(Y[i] - Y[j])]++;
+	for (auto i = m1.begin(); i != m1.end(); i++)
+		ans += m2.find(i->first) != m2.end();
+	return ans;
+}
+
+bool search(int Y[], int i, int m)
+{
+	int l = 0, u = m - 1;
+	while (l <= u)
+	{
+		if (Y[(l + u) / 2] == i)
+			return true;
+		else if (Y[(l + u) / 2] < i)
+			l = ((l + u) / 2) + 1;
+		else
+			u = ((l + u) / 2) - 1;
+	}
+	return false;
+}
+
+int32_t main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	int w, h, n, m;
+	cin >> w >> h >> n >> m;
+	vi A;
+	int X[10000], Y[10000];
+	for (int i = 0; i < n; i++)
+		cin >> X[i];
+	for (int i = 0; i < m; i++)
+		cin >> Y[i];
+	sort(all(X, n));
+	sort(all(Y, m));
+	int x = X[n - 1] - X[0] + Y[m - 1];
+	A.pb(numberOfSquares(X, Y, n, m + 1));
+	int i = 1;
+	while (i <= h)
+	{
+		if (!search(Y, i, m))
+		{
+			Y[m] = i;
+			A.pb(numberOfSquares(X, Y, n, m + 1));
+		}
+		i++;
+	}
+	cout << *max_element(A.begin(), A.end());
+	return 0;
+}
+int power(int x, unsigned int y)
+{
+	int res = 1;
+	x = x % mod;
+	if (x == 0)
+		return 0;
+	while (y > 0)
+	{
+		if (y & 1)
+			res = (res * x) % mod;
+		y = y >> 1;
+		x = (x * x) % mod;
+	}
+	return res;
+}
